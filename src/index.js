bd87a92dbe773cef0143dd5655dce82a48b8ed7a
checkMySwag <levchukcryptoeugene@gmail.com>
2019-02-19 21:57:09 +0300

    solution brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let brStrArr = [];
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    brStrArr.push(bracketsConfig[i].join(''));
+  }
+  let k = Math.floor(str.length / 2);
+  while (k) {
+    for (let j = 0; j < brStrArr.length; j++) {
+      if (str.indexOf(brStrArr[j]) !== -1) {
+        str = str.replace(brStrArr[j], "");
+      }
+    }
+    k--;
+  }
+  if (str === "") {
+    return true;
+  } else {
+    return false;
+  }
 }
